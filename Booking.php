14245111c72f9735cff45b386fe704352a992f9e
GresaDodaj <34664145+gresad@users.noreply.github.com>
2018-06-06 14:32:22 +0200

    cmt1 Booking

--- Booking.php
+++ Booking.php
@@ -10,6 +10,37 @@
    <script src="jquery-1.11.2.min.js">  </script>
     <style>
 
+
+.im1{
+    background:url('Fotot/img1.jpg');
+    background-repeat:no-repeat;
+    background-size:100% 100%;
+    width:100%;
+    height:100%;
+        }
+.im2{
+      background:url('Fotot/img2.jpg');
+      background-repeat:no-repeat;
+      background-size:100% 100%;
+      width:100%;
+      height:100%;
+    }
+.im3{
+  background:url('Fotot/img3.jpg');
+  background-repeat:no-repeat;
+  background-size:100% 100%;
+  width:100%;
+  height:100%;
+    }
+.im4{
+  background:url('Fotot/img4.jpg');
+  background-repeat:no-repeat;
+  background-size:100% 100%;
+  width:100%;
+  height:100%;
+    }
+
+
         * {
             font-family: 'Trebuchet ms'
         }
@@ -100,59 +131,23 @@
 <div style="width:31.5%;height:28.7em;float:left; ">
     <div style=" margin-left:45.5%;;margin-top:24%;height:50%;width:77%;">
 
-        <div class="hi"> </div>
+        <div class="im1"> </div>
 
 </div>
 <div  style="margin-top:20% ; margin-left:45.5%;height:50%;width:77%;" >
-        <div class="hi1"> </div>
+        <div class="im2"> </div>
     
 </div>
 <div  style="margin-top:20% ; margin-left:45.5%;height:50%;width:77%;" >
-    <div class="hi2"> </div>
+    <div class="im3"> </div>
 
 </div>
-
 
 </div>
     
-
 
     <div class="dropdown" style="margin-top:5.5% ; margin-left:560px;height:35%;width:45%;" >   
     <article id="transparente">
-    <?php
-                            GLOBAL $emailErr;
-                            GLOBAL $email;
-                            GLOBAL $nameErr;
-                            GLOBAL $name;
-                            if (empty($_POST["name"])) {
-                                $nameErr = "Username is required";
-                              } else {
-                                $name = test_input($_POST["name"]);
-                                // check if name only contains letters and whitespace
-                                if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-                                  $nameErr = "Only letters and white space allowed"; 
-                                }
-                              }
-                if (empty($_POST["email"])) {
-                    $emailErr = "Email is required";
-                } else {
-                    $email = test_input($_POST["email"]);
-                    // check if e-mail address is well-formed
-                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $emailErr = "Invalid email format"; 
-                    }
-                }
-
-
-                function test_input($data) {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
-                }
-                ?>
-
-
 
 
 
@@ -163,7 +158,7 @@
     <label>Username:</label>
    <!-- Cfaredo username qe permban vetem shkronja dhe te pakten 3-->
     <input  name="name"  autofocus="" placeholder="Username"   value="<?php echo $name;?>">
-    <span class="error">* <?php echo $nameErr;?></span><br><br>
+    <span class="error">* </span><br><br>
     <label>First Name:</label>
     <input type="text" name="firstname" required="" placeholder="First Name">
     <br><br>
@@ -171,8 +166,8 @@
     <input type="text" name="lastname" required="" placeholder="Last Name">
     <br><br>
     <label>E-mail:</label>
-    <input  name="email" id="email"  placeholder="e-mail" title="Please enter a valid email address" value="<?php echo $email; ?>">
-* <?PHP echo $emailErr; ?><br><br>
+    <input  name="email" id="email"  placeholder="e-mail" title="Please enter a valid email address">
+* <br><br>
      <label>Phone Number:</label>
      <input type="tel" name="phone" id="phone" required placeholder="Please enter your phone number ">
      <br><br>
